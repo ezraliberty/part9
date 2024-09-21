1d9b9f32b9f13ab63589d9e3b40eeda9b0a5e14f
ezraliberty <ezraliberty99@gmail.com>
2024-09-22 00:49:41 +0100

    fix node env argv

--- back/multiplier.ts
+++ back/multiplier.ts
@@ -2,4 +2,7 @@ const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText,  a * b);
 }
 
-multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+
+multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`);
