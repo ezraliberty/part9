2386fdb58be320b3b45db164e822a2b306e7e8d3
ezraliberty <ezraliberty99@gmail.com>
2024-09-21 23:03:55 +0100

    ts intro

---
+++ back/multiplier.ts
@@ -0,0 +1,18 @@
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText,  a * b);
+}
+
+multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
+
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation) => {
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) return 'can\'t divide by 0!';
+    return a / b;
+  }
+}
