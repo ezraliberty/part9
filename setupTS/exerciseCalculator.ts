dddabdeb41cd57d36ad30e6bc3a4626572f9511e
ezraliberty <ezraliberty99@gmail.com>
2024-09-23 13:45:10 +0100

    entry calc

--- setupTS/exerciseCalculator.ts
+++ setupTS/exerciseCalculator.ts
@@ -1,1 +1,16 @@
-function calculateExercises
+interface CalculatedValue {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+function calculateExercises(hours: number[], target: number) : CalculatedValue {
+    const periodLength = hours.length;
+    const trainingDays = hours.filter(h => h > 0).length
+    const success = hours.length > 6;
+    const rating = 
+}
