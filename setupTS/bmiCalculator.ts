881c15dbf96f83b4c9a04bdf44f6420878ef5486
ezraliberty <ezraliberty99@gmail.com>
2024-09-23 13:05:07 +0100

    bmi calculator

---
+++ setupTS/bmiCalculator.ts
@@ -0,0 +1,18 @@
+function calculateBmi(height: number, weight: number): string {
+    const m = height /100;
+    const bmi = weight / (m * m);
+    if (bmi < 18.5) {
+        return "Underweight range"
+    } else if (bmi < 24.9) {
+        return "Normal range"
+    } else if (bmi < 29.9) {
+        return "Overweight range"
+    } else {
+        return "Obese range"
+    }
+}
+
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+
+console.log(calculateBmi(height, weight))
