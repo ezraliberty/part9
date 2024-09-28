e4fe93f991e5cd9d79a8d63dec960bec72232e59
ezraliberty <ezraliberty99@gmail.com>
2024-09-28 18:47:37 +0100

    webBMI

--- setupTS/bmiCalculator.ts
+++ setupTS/bmiCalculator.ts
@@ -1,4 +1,4 @@
-function calculateBmi(height: number, weight: number): string {
+export function calculateBmi(height: number, weight: number): string {
     const m = height /100;
     const bmi = weight / (m * m);
     if (bmi < 18.5) {
--- setupTS/index.ts
+++ setupTS/index.ts
@@ -1,8 +1,23 @@
 import express from "express"
+import { calculateBmi } from "./bmiCalculator"
 const app = express()
+
 
 app.get("/hello", (_req, res) => {
     res.send("Hello Full Stack!")
+})
+
+app.get("/bmi", (req, res) => {
+    const weight = Number(req.query.weight);
+    const height = Number(req.query.height);
+
+    if (isNaN(height) || isNaN(weight)) {
+        res.status(400).json({error: 'malformatted parameters'});
+        return;
+    }
+
+    const bmi = calculateBmi(weight, height);
+    res.json({ weight, height, bmi });
 })
 
 const PORT = 3006
