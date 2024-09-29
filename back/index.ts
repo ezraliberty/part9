3c24d4574a45db887e92336e7afcda1c8ab83547
ezraliberty <ezraliberty99@gmail.com>
2024-09-29 04:49:40 +0100

    type config

--- back/index.ts
+++ back/index.ts
@@ -1,12 +1,28 @@
 import express from "express";
-const app = express()
+import { calculator, Operation } from "./calculator";
+const app = express();
+
+app.use(express.json());
 
 app.get("/ping", (_req, res) => {
-    res.send('pong')
-})
+    res.send('pong');
+});
 
-const PORT = 3003
+app.post("/calculate", (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { value1, value2, op } = req.body;
+
+    if (!value1 || isNaN(Number(value1))) {
+        return res.status(400).send({ error: '...' });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculator(Number(value1), Number(value2), op as Operation);
+    res.send({ result });
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
-    console.log(`Server is runnig on PORT: ${PORT}`)
-})
+    console.log(`Server is runnig on PORT: ${PORT}`);
+});
