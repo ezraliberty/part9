dc5e9dd5b38127b3adf6cf4ed4a5122b4dfd6e5e
ezraliberty <ezraliberty99@gmail.com>
2024-10-04 07:05:13 +0100

    service

--- diary/src/index.ts
+++ diary/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import diaryRouter from "./routes/diaries";
 const app = express();
 
 app.use(express.json());
+app.use("api/diaries", diaryRouter);
 
 const PORT = 3000;
 
